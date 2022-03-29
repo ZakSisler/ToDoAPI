d99e150c524edee81d7f27b110787ce06d7b79f8
Zakary Sisler <zakarysisler@outlook.com>
2022-03-29 16:17:23 -0500

    I added my controllers and made edits to my web config

---
+++ ToDoAPI.API/Controllers/CategoriesController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDoAPI.API.Models;
+using ToDoAPI.DATA.EF;
+using System.Web.Http.Cors;
+
+namespace ToDoAPI.API.Controllers
+{
+    //GET = READ
+    //POST = CREATE
+    //PUT = EDIT
+    //DELETE = DELETE
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class CategoriesController : ApiController
+    {
+        ToDoEntities db = new ToDoEntities();
+
+        //GET - /api/resources
+        public IHttpActionResult GetCats()
+        {
+            //Below we create a list of EF resource objects. In an API, it's best practice to install EF to the API layer when needing to accomplish this task.
+            List<CategoryViewModel> cat = db.Categories.Include("Category").Select(c => new CategoryViewModel()
+            {
+                //Assign the columns of the Resources db table to the ResourceViewModel object, so we can use the data (send the data back to requesting app)
+                CategoryId = c.CategoryId,
+                Name = c.Name,
+                Description = c.Description
+
+            }).ToList<CategoryViewModel>();
+
+            //Check the results and handle accordingly below
+            if (cat.Count == 0)
+            {
+                return NotFound();
+            }
+            //Everything is good, return the data
+            return Ok(cat);//resources are being passed in the response back to the requesting app.
+        }//end GetResources()
+
+        //GET - api/resources/id
+        public IHttpActionResult GetCat(int id)
+        {
+            //Create a new REsourceViewModel object and assign it to the appropriate resource from the db
+            CategoryViewModel cat = db.Categories.Where(c => c.CategoryId == id).Select(c =>
+                new CategoryViewModel()
+                {
+                    //Coopy the assignments from the GetResources() and paste below
+
+                    CategoryId = c.CategoryId,
+                    Name = c.Name,
+                    Description = c.Description
+
+                }).FirstOrDefault();
+            //scopeless if - once the return executes the scopes are closed.
+            if (cat == null)
+                return NotFound();
+
+            return Ok(cat);
+
+        }//end GetResource
+
+        //POST - api/Resources (HttpPost)
+        public IHttpActionResult PostCat(CategoryViewModel cat)
+        {
+            //1. Check to validate the object - we need to know that all the data necessary to create a resource is there.
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid Data");
+            }
+
+            Category newCat = new Category()
+            {
+                CategoryId = cat.CategoryId,
+                Name = cat.Name,
+                Description = cat.Description
+            };
+
+            //add a record and save changes
+            db.Categories.Add(newCat);
+            db.SaveChanges();
+
+            return Ok(newCat);
+
+        }
+
+        //PUT - api/resources (HTTPPut)
+        public IHttpActionResult PutCat(CategoryViewModel cat)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid Data");
+            }
+
+            //We get the resource from the db so we can modify it
+            Category existingCat = db.Categories.Where(c => c.CategoryId == cat.CategoryId).FirstOrDefault();
+
+            if (existingCat != null)
+            {
+                existingCat.CategoryId = cat.CategoryId;
+                existingCat.Name = cat.Name;
+                existingCat.CategoryId = cat.CategoryId;
+                db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+
+        //DELETE - api/Resources/id (HTTPDelete)
+        public IHttpActionResult DeleteCat(int id)
+        {
+            //Get resource from the api to make sure theres a resource with this id
+            Category cat = db.Categories.Where(c => c.CategoryId == id).FirstOrDefault();
+
+            if (cat != null)
+            {
+                db.Categories.Remove(cat);
+                db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
---
+++ ToDoAPI.API/Controllers/ToDoController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDoAPI.API.Models;
+using ToDoAPI.DATA.EF;
+using System.Web.Http.Cors;
+
+namespace ToDoAPI.API.Controllers
+{
+    //GET = READ
+    //POST = CREATE
+    //PUT = EDIT
+    //DELETE = DELETE
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ToDoController : ApiController
+    {
+        //Create a connection to the db
+        ToDoEntities db = new ToDoEntities();
+
+        //GET - /api/resources
+        public IHttpActionResult GetToDos()
+        {
+            //Below we create a list of EF resource objects. In an API, it's best practice to install EF to the API layer when needing to accomplish this task.
+            List<ToDoViewModel> toDos = db.ToDoItems.Include("Category").Select(t => new ToDoViewModel()
+            {
+                //Assign the columns of the Resources db table to the ResourceViewModel object, so we can use the data (send the data back to requesting app)
+                TodoId = t.TodoId,
+                Action = t.Action,
+                Done = t.Done,
+                CategoryId = t.CategoryId,
+                Category = new CategoryViewModel()
+                {
+                    CategoryId = t.Category.CategoryId,
+                    Name = t.Category.Name,
+                    Description = t.Category.Description
+                }
+            }).ToList<ToDoViewModel>();
+
+            //Check the results and handle accordingly below
+            if (toDos.Count == 0)
+            {
+                return NotFound();
+            }
+            //Everything is good, return the data
+            return Ok(toDos);//resources are being passed in the response back to the requesting app.
+        }//end GetResources()
+
+        //GET - api/resources/id
+        public IHttpActionResult GetToDo(int id)
+        {
+            //Create a new REsourceViewModel object and assign it to the appropriate resource from the db
+            ToDoViewModel toDos = db.ToDoItems.Include("Category").Where(t => t.TodoId == id).Select(t =>
+                new ToDoViewModel()
+                {
+                    //Coopy the assignments from the GetResources() and paste below
+                    TodoId = t.TodoId,
+                    Action = t.Action,
+                    Done = t.Done,
+                    CategoryId = t.CategoryId,
+                    Category = new CategoryViewModel()
+                    {
+                        CategoryId = t.Category.CategoryId,
+                        Name = t.Category.Name,
+                        Description = t.Category.Description
+                    }
+                }).FirstOrDefault();
+            //scopeless if - once the return executes the scopes are closed.
+            if (toDos == null)
+                return NotFound();
+
+            return Ok(toDos);
+
+        }//end GetResource
+
+        //POST - api/Resources (HttpPost)
+        public IHttpActionResult PostToDo(ToDoViewModel todo)
+        {
+            //1. Check to validate the object - we need to know that all the data necessary to create a resource is there.
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid Data");
+            }
+
+            ToDoItem newToDo = new ToDoItem()
+            {
+                Action = todo.Action,
+                Done = todo.Done,
+                CategoryId = todo.CategoryId
+            };
+
+            //add a record and save changes
+            db.ToDoItems.Add(newToDo);
+            db.SaveChanges();
+
+            return Ok(newToDo);
+
+        }
+
+        //PUT - api/resources (HTTPPut)
+        public IHttpActionResult PutToDo(ToDoViewModel todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid Data");
+            }
+
+            //We get the resource from the db so we can modify it
+            ToDoItem existingToDo = db.ToDoItems.Where(r => r.TodoId == todo.TodoId).FirstOrDefault();
+
+            if (existingToDo != null)
+            {
+                existingToDo.TodoId = todo.TodoId;
+                existingToDo.Action = todo.Action;
+                existingToDo.CategoryId = todo.CategoryId;
+                db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+        //DELETE - api/Resources/id (HTTPDelete)
+        public IHttpActionResult DeleteToDo(int id)
+        {
+            //Get resource from the api to make sure theres a resource with this id
+            ToDoItem toDo = db.ToDoItems.Where(t => t.TodoId == id).FirstOrDefault();
+
+            if (toDo != null)
+            {
+                db.ToDoItems.Remove(toDo);
+                db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+
+        //We use Dispose() below to dispose of any connection to the database after we are done with them - best practice to handle performance - dispose of the instance of the controller and db connection when we are done with it.
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+    }//end class
+}//end namespace
